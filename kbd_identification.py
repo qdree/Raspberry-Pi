ed21683677a7fe992ed53e378c348e06b11673d3
qdree <ta4ckpih@gmail.com>
2016-09-26 12:49:07 +0300

    cleaned and commentedv_1

---
+++ kbd_identification.py
@@ -0,0 +1,32 @@
+import pyudev 
+import evdev 
+import functools
+import re
+
+target_device_data = [] #list to store recieved data about device
+
+context = pyudev.Context()
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by(subsystem = 'input')
+
+monitor.start()
+
+while True:
+	for device in iter(functools.partial(monitor.poll, 0), None):
+		#print ('{0.action} on {0.device_path}'.format(device))
+		#print (type(device.action))
+
+		target_device_data.append(device.device_path) #data collection
+		try:
+			if device.action == "add":
+				for dev in target_device_data:
+					splitted = dev.split('/')
+					target = splitted[-1]
+					if re.match('event[0-9+]',target): #check of event.. param.
+						added_dev = str(evdev.InputDevice('/dev/input/{0}'.format(target))).split('/') #data collection about target device
+						#print (added_dev)
+						#re_addded_dev  = re.findall('.*name ".* Keyboard".*', added_dev[3])
+						if re.match('.*name ".* Keyboard".*', added_dev[3]): #check if target device is keyboard
+							print ('Keyboard added')
+		except Exception as e:
+			print (e)
