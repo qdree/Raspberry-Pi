8b43fb8f8ef7bc5ae8d5faab25b6bae5b28bd86a
qdree <ta4ckpih@gmail.com>
2016-09-27 17:38:41 +0300

    pretest version of script

---
+++ mov_access_from_fdrive.py
@@ -0,0 +1,26 @@
+# import os
+# import pyudev
+# import evdev
+# import re
+
+
+
+# movie = ("/media/pi/{0}")
+
+
+import os
+import glob
+import sys
+# curDir = sys.argv[1]
+curDir = os.getcwd()
+for dirName, subdirList, fileList in os.walk(curDir):
+    #print('Found directory: %s' % dirName)
+    for fname in fileList:
+        fullName = os.path.join(dirName, fname)
+        print fullName
+    #     try:
+    #        if fname.split(".")[-1] == "bin" and len(fname) > 15:
+    #           os.rename(fullName, os.path.join(dirName, fname[:-15]+".bin"))
+    #           print fullName +": ok"
+    #     except OSError, e:
+    #        print e + " for" + fullName
