ce467a91aad4a309d2e0921f3cab4b43863bef1f
qdree <ta4ckpih@gmail.com>
2016-09-28 18:07:46 +0300

    almost working. bug with format avoidness at the name input

--- mov_access_from_fdrive.py
+++ mov_access_from_fdrive.py
@@ -13,6 +13,7 @@ def videoNameSet():
 	try: #if logfile present
 		with open('logfile.txt', 'r+') as logfile:
 			logfile.write(video_name)
+			logfile.flush()
 			for line in logfile: #iterate though logfile
 				splitted = line.split('.') #create pure video name - without format
 				#print (splitted)
@@ -24,35 +25,31 @@ def videoNameSet():
 
 	except IOError: #if no logfile
 		with open('logfile.txt', 'w') as logfile: #create file
+			video_name.split(".")
 			logfile.write(video_name)
-		video_name.split(".") 
+			logfile.flush() 
 		return video_name
+
 	
-	#print (video_name)
 
 
 def pathCreation(video_name):
 	pattern = re.compile(r'.*mp4 | .*mpeg | .*avi', flags = re.I | re.X | re.U) #pattern for regex
 	os.chdir('/media/')
 	try:
-		for file in os.walk(os.getcwd()): #iter through generator of pathes
-			# re_file = str(pattern.findall(str(file))).split('/') #pull of last file in dir and subdir
-			re_file = str(pattern.findall(str(file))) #pull of last file in dir and subdir
-			# print (re_file) #*******debug**********
-			file_name = re_file[-1].split('.') #create pure video name - without format
-			print (file_name) #*****debug**********
-			if video_name == file_name: #check chosen file with gotten list
-				# return re_file[-1]
-				cwd = os.getcwd() #current working directory
-				path_to_video = cwd + re_file
-				#print (cwd) #*******debug*******
-				return path_to_video
+		for dirName, curdirList, fileList in os.walk(os.getcwd()): #iterate through generator of pathes
+			for file in fileList:
+				full_path = os.path.join(dirName, file) #full path to file creation
+				re_file = str(pattern.findall(str(full_path))).split('/') #get all files matching the pattern 
+				if len(re_file) > 1:
+					file_name = re_file[-1].split('.')[0] #pure file name - without format
+					print (file_name)
+					if video_name == file_name: #check chosen file with gotten list
+						print (full_path)
+						return full_path
 
 	except Exception as e:
 		print (e)
 
 
-
-# pathCreation(videoNameSet())
-#videoNameSet
-pathCreation("SampleVideo")
+pathCreation(videoNameSet())
