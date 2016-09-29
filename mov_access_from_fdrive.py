b4bfb210b847437bf6751082abdfd30d9346c1a6
qdree <ta4ckpih@gmail.com>
2016-09-29 17:39:13 +0300

    problems with lambda func, as a result have to use traditional func to clear file

--- mov_access_from_fdrive.py
+++ mov_access_from_fdrive.py
@@ -3,6 +3,10 @@ import pyudev
 import evdev
 import re
 
+def cleanContent(fName):
+	fName.seek(0)
+	fName.truncate()
+
 def videoNameSet():
 	#input of file name
 	try:
@@ -18,7 +22,8 @@ def videoNameSet():
 				if line == pure_name: #check name is the same as in log
 					return line
 				else:
-					lambda logfile: logfile.seek(0), logfile.truncate() #func for file's content cleanup
+					#lambda logfile: logfile.seek(0), logfile.truncate() #func for file's content cleanup
+					cleanContent(logfile) #func for file's content cleanup
 					logfile.write(pure_name)
 					logfile.flush()
 					return pure_name
@@ -31,7 +36,7 @@ def videoNameSet():
 
 
 def pathCreation(video_name):
-	pattern = re.compile(r'(.*\.mp4) | (.*\.mpeg) | (.*\.avi)', flags = re.I | re.X | re.U) #pattern for regex
+	pattern = re.compile(r'(.*\.mp4) | (.*\.mpeg) | (.*\.avi) | (.*\.mkv)', flags = re.I | re.X | re.U) #pattern for regex
 	os.chdir('/media/')
 	try:
 		for dirName, curdirList, fileList in os.walk(os.getcwd()): #iterate through generator of pathes
@@ -43,6 +48,7 @@ def pathCreation(video_name):
 					file_name = file_full_name.split('.')[0] #pure name without format
 					print ("Found : {0} located in :{1}".format(file_full_name, full_path))
 
+					print video_name, file_name
 					if video_name == file_name.lower(): #check chosen file with files from a list
 						print ('Path to target file : {0}'.format(full_path))
 						return full_path
