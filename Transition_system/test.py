618816ed00e2e7b247f94e9ac2dbe4c4b12a8f7e
qdree <ta4ckpih@gmail.com>
2016-10-04 11:47:09 +0300

    fist demo of project, tested on ubuntu

--- Transition_system/test.py
+++ Transition_system/test.py
@@ -1,5 +1,24 @@
-from videoAccess import pathCreation, videoNameSet
+from videoAccess import *
+from kbd_identification import *
 
-name = raw_input("Input name:")
+kbd_found = False
+password = "arsenal"
 
-path = pathCreation(videoNameSet(name))
+#video setup before program workflow
+input_name = raw_input("Input name:")
+try:
+	pathCreation(nameCheck(input_name))
+except Exception as e:
+	print e
+
+#keyboard identification
+while not kbd_found:
+	while not kbdIden():
+		if kbdIden():
+			print 'Keyboard added'
+			kbd_found = True
+
+input_password = raw_input("PASSWORD: ")
+
+if input_password.lower() == password:
+	print "Correct!!!!"
