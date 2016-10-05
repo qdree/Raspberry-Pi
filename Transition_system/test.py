9a134e295a420a97151e029cd7c3be2e4e5b4822
qdree <ta4ckpih@gmail.com>
2016-10-05 11:34:15 +0300

    playback correction

--- Transition_system/test.py
+++ Transition_system/test.py
@@ -26,25 +26,25 @@ movie = (path)
 while not kbdIden():
 		pass
 print "Keyboard added"
+
 GPIO.output(18, GPIO.HIGH) #turn off LED
 
 #password check
 input_password = raw_input("ENTER PASSWORD:")
 while not input_password == password:
 	input_password = raw_input("ENTER PASSWORD:")
-
 print ("Correct password!")
 
 #	if not active:
 		# os.system('vcgencmd display_power 1') #screen monitor switcher //ON
 		# time.sleep(1.5)
 #active = True
-omxc = os.popen("omxplayer -b %s" %movie)
-print ("\nplayer started")
-player_active = True
-#time.sleep(2)
-
-if (player_active and os.system("pidof omxplayer.bin") == 256):
+if not player_active:
+	omxc = os.popen("omxplayer -b %s" %movie)
+	print ("\nplayer started")
+	player_active = True
+	#time.sleep(2)
+elif (player_active and os.system("pidof omxplayer.bin") == 256):
 	print ("\nplayer is inactive")
 	player_active = False
 #	active = False
